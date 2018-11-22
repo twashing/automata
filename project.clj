67837ac42b76c2622fe8de4047b1415414c1dac8
Timothy Washington <twashing@interruptsoftware.com>
2018-11-22 12:32:24 -0800

    Adding a "releases" :repository

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject automata "0.1.1"
+(defproject automata "0.1.2-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -7,7 +7,14 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/test.check "0.10.0-alpha3"]]
 
+  :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.9"]]
+                   :source-paths ["src" "workbench"]}}
+
   :plugins [[lein-pprint "1.2.0"]]
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :username :env
+                              :sign-releases false
+                              :password :env}]]
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
@@ -15,7 +22,4 @@
                   ["deploy"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
-                  ["vcs" "push"]]
-
-  :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.9"]]
-                   :source-paths ["src" "workbench"]}})
+                  ["vcs" "push"]])
