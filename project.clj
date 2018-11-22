90c5d0a83e901a68acbda0c76d828afbe200e1f8
Timothy Washington <twashing@interruptsoftware.com>
2018-11-22 12:26:24 -0800

    Adding custom :release-tasks, so as not to sign tags

--- project.clj
+++ project.clj
@@ -3,8 +3,19 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/test.check "0.10.0-alpha3"]]
+
   :plugins [[lein-pprint "1.2.0"]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "v" "--no-sign"] ; disable signing and add "v" prefix
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
+
   :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.9"]]
                    :source-paths ["src" "workbench"]}})
