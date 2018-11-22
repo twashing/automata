5202e7205d51bcabab9b04d66a328c94d3a1e5c1
Timothy Washington <twashing@interruptsoftware.com>
2018-11-21 18:18:05 -0800

    Adding tests for scalar + star combinators

--- src/automata/core.clj
+++ src/automata/core.clj
@@ -176,18 +176,39 @@
   (-> a (advance :a) (advance :a)) ;; error
   (-> a (advance :a) (advance :b)))
 
-
 (comment
 
-  (do
-    (def b (automaton [(* :a) :b :c :d]))
-    (def c (automaton [:a (* :b) :c :d]))
-    (def d (automaton [(* :a) (* :b) :c :d])))
-
+  (def b (automaton [(* :a) :b :c :d]))
 
   (-> b (advance :a))
   (-> b (advance :a) (advance :a))
   (-> b (advance :a) (advance :b))
-  (-> b (advance :b))
+  (-> b (advance :b)))
 
+(comment
+
+  (def c (automaton [:a (* :b) :c :d]))
+
+  (-> c (advance :a))
+  (-> c (advance :a) (advance :b) (advance :b))
+
+
+  ;; TODO fix
+  (-> c (advance :a) (advance :c))
+  (-> c (advance :b))
+  (-> c (advance :d)))
+
+(comment
+
+  (def d (automaton [(* :a) (* :b) :c :d]))
+
+  (-> d (advance :a))
+  (-> d (advance :a) (advance :a))
+  (-> d (advance :a) (advance :b))
+  (-> d (advance :b) (advance :b))
+  (-> d (advance :b) (advance :b) (advance :c))
+  (-> d (advance :c))
+
+  ;; TODO fix
+  (-> d (advance :d)) ;; error
   )
--- test/automata/core_test.clj
+++ test/automata/core_test.clj
@@ -2,6 +2,191 @@
   (:require [clojure.test :refer :all]
             [automata.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest test-scalars
+
+  (let [a (automaton [:a :b :c :d])]
+
+    (testing "basic scalar matches"
+
+      (is (= (advance a :a)
+             {:states '(#automata.core.Scalar{:matcher :a} #automata.core.Scalar{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Scalar{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :state #automata.core.Scalar{:matcher :a}
+              :history '(nil {:state #automata.core.Scalar{:matcher :a} :input :a :transition :automata.core/match})}))
+
+      (is (= (-> a (advance :a) (advance :b))
+             {:states '(#automata.core.Scalar{:matcher :a} #automata.core.Scalar{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :state #automata.core.Scalar{:matcher :b}
+              :history '(nil
+                          {:state #automata.core.Scalar{:matcher :a} :input :a :transition :automata.core/match}
+                          {:state #automata.core.Scalar{:matcher :b} :input :b :transition :automata.core/match})})))
+
+    (testing "basic scalar match errors"
+
+      (is (= (advance a :b)
+             {:states '(#automata.core.Scalar{:matcher :a} #automata.core.Scalar{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Scalar{:matcher :a} #automata.core.Scalar{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :state nil
+              :history [nil]
+              :error {:type :invalid-trasition :input :b :matcher #automata.core.Scalar{:matcher :a}}}))
+
+      (is (= (-> a (advance :a) (advance :a))
+             {:states '(#automata.core.Scalar{:matcher :a} #automata.core.Scalar{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Scalar{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :state #automata.core.Scalar{:matcher :a}
+              :history '(nil {:state #automata.core.Scalar{:matcher :a} :input :a :transition :automata.core/match})
+              :error {:type :invalid-trasition :input :a :matcher #automata.core.Scalar{:matcher :a}}})))))
+
+(deftest test-star-basic
+
+  (let [b (automaton [(* :a) :b :c :d])]
+
+    (testing "single star"
+
+      (is (= (-> b (advance :a))
+             {:states '(#automata.core.Star{:matcher :a} #automata.core.Scalar{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Scalar{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :state #automata.core.Star{:matcher :a}
+              :history '(nil {:state #automata.core.Star{:matcher :a} :input :a :transition :automata.core/match})})))
+
+    (testing "many stars"
+
+      (is (= (-> b (advance :a) (advance :a))
+             {:states '(#automata.core.Star{:matcher :a} #automata.core.Scalar{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Scalar{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :state #automata.core.Star{:matcher :a}
+              :history
+              '(nil
+                 {:state #automata.core.Star{:matcher :a} :input :a :transition :automata.core/match}
+                 {:state #automata.core.Star{:matcher :a} :input :a :transition :automata.core/match})})))
+
+    (testing "star then scalar"
+
+      (is (= (-> b (advance :a) (advance :b))
+             {:states '(#automata.core.Star{:matcher :a} #automata.core.Scalar{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :state #automata.core.Scalar{:matcher :b}
+              :history
+              '(nil
+                 {:state #automata.core.Star{:matcher :a} :input :a :transition :automata.core/match}
+                 {:state #automata.core.Scalar{:matcher :b} :input :b :transition :automata.core/match})})))
+
+    (testing "bypass star"
+
+      (is (= (-> b (advance :b))
+             {:states '(#automata.core.Star{:matcher :a} #automata.core.Scalar{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :state #automata.core.Scalar{:matcher :b}
+              :history
+              '(nil
+                 {:state #automata.core.Star{:matcher :a} :input :b :transition :automata.core/noop}
+                 {:state #automata.core.Scalar{:matcher :b} :input :b :transition :automata.core/match})})))))
+
+(deftest test-star-surrounded
+
+  (let [c (automaton [:a (* :b) :c :d])]
+
+    (testing "scalar then star"
+
+      (is (= (-> c (advance :a))
+             {:states '(#automata.core.Scalar{:matcher :a} #automata.core.Star{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Star{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :state #automata.core.Scalar{:matcher :a}
+              :history '(nil {:state #automata.core.Scalar{:matcher :a} :input :a :transition :automata.core/match})})))
+
+    (testing "scalar then star (many)"
+
+      (is (= (-> c (advance :a) (advance :b) (advance :b))
+             {:states '(#automata.core.Scalar{:matcher :a} #automata.core.Star{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :state #automata.core.Star{:matcher :b}
+              :history
+              '(nil
+                 {:state #automata.core.Scalar{:matcher :a} :input :a :transition :automata.core/match}
+                 {:state #automata.core.Star{:matcher :b} :input :b :transition :automata.core/match}
+                 {:state #automata.core.Star{:matcher :b} :input :b :transition :automata.core/match})})))
+
+    ;; TODO fix
+    ;; (-> c (advance :a) (advance :c))
+
+    (testing "pre-star matching error"
+
+      (is (= (-> c (advance :b))
+             {:states '(#automata.core.Scalar{:matcher :a} #automata.core.Star{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Scalar{:matcher :a} #automata.core.Star{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :state nil
+              :history [nil]
+              :error {:type :invalid-trasition :input :b :matcher #automata.core.Scalar{:matcher :a}}}))
+
+      (is (= (-> c (advance :d))
+             {:states '(#automata.core.Scalar{:matcher :a} #automata.core.Star{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Scalar{:matcher :a} #automata.core.Star{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :state nil
+              :history [nil]
+              :error {:type :invalid-trasition :input :d :matcher #automata.core.Scalar{:matcher :a}}})))))
+
+(deftest test-star-multiple
+
+  (let [d (automaton [(* :a) (* :b) :c :d])]
+
+    (testing "single advance"
+      (is (= (-> d (advance :a))
+             {:states '(#automata.core.Star{:matcher :a} #automata.core.Star{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Star{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :state #automata.core.Star{:matcher :a}
+              :history '(nil {:state #automata.core.Star{:matcher :a} :input :a :transition :automata.core/match})})))
+
+    (testing "multiple advance"
+      (is (= (-> d (advance :a) (advance :a))
+             {:states '(#automata.core.Star{:matcher :a} #automata.core.Star{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Star{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :state #automata.core.Star{:matcher :a}
+              :history '(nil
+                          {:state #automata.core.Star{:matcher :a} :input :a :transition :automata.core/match}
+                          {:state #automata.core.Star{:matcher :a} :input :a :transition :automata.core/match})})))
+
+    (testing "first then second advance"
+      (is (= (-> d (advance :a) (advance :b))
+             {:states '(#automata.core.Star{:matcher :a} #automata.core.Star{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :state #automata.core.Star{:matcher :b}
+              :history '(nil
+                          {:state #automata.core.Star{:matcher :a} :input :a :transition :automata.core/match}
+                          {:state #automata.core.Star{:matcher :b} :input :b :transition :automata.core/match})})))
+
+    (testing "skip first multiple advance second"
+      (is (= (-> d (advance :b) (advance :b))
+             {:states '(#automata.core.Star{:matcher :a} #automata.core.Star{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :state #automata.core.Star{:matcher :b}
+              :history '(nil
+                          {:state #automata.core.Star{:matcher :a} :input :b :transition :automata.core/noop}
+                          {:state #automata.core.Star{:matcher :b} :input :b :transition :automata.core/match}
+                          {:state #automata.core.Star{:matcher :b} :input :b :transition :automata.core/match})})))
+
+    (testing "skip first multiple advance second third"
+      (is (= (-> d (advance :b) (advance :b) (advance :c))
+             {:states '(#automata.core.Star{:matcher :a} #automata.core.Star{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Scalar{:matcher :d})
+              :state #automata.core.Scalar{:matcher :c}
+              :history '(nil
+                          {:state #automata.core.Star{:matcher :a} :input :b :transition :automata.core/noop}
+                          {:state #automata.core.Star{:matcher :b} :input :b :transition :automata.core/match}
+                          {:state #automata.core.Star{:matcher :b} :input :b :transition :automata.core/match}
+                          {:state #automata.core.Scalar{:matcher :c} :input :c :transition :automata.core/match})})))
+
+    (testing "skip first + second single advance"
+      (is (= (-> d (advance :c))
+             {:states '(#automata.core.Star{:matcher :a} #automata.core.Star{:matcher :b} #automata.core.Scalar{:matcher :c} #automata.core.Scalar{:matcher :d})
+              :run '(#automata.core.Scalar{:matcher :d})
+              :state #automata.core.Scalar{:matcher :c}
+              :history '(nil
+                          {:state #automata.core.Star{:matcher :a} :input :c :transition :automata.core/noop}
+                          {:state #automata.core.Star{:matcher :b} :input :c :transition :automata.core/noop}
+                          {:state #automata.core.Scalar{:matcher :c} :input :c :transition :automata.core/match})}))))
+
+  ;; TODO fix
+  ;; (-> d (advance :d)) ;; error
+  )
