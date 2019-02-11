65b9c01adfd6e84a1413c698340033e6e971b4ca
Timothy Washington <twashing@interruptsoftware.com>
2019-02-11 14:43:23 +0200

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject automata "0.2.0-SNAPSHOT"
+(defproject automata "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
